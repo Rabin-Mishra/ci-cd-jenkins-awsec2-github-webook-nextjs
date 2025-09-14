a1ebb58208e585020c0b43cf27559179cada657a
Rabin Mishra <mishrarabin92@gmail.com>
2025-09-14 18:37:56 +0545

    changes made to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,9 @@ export default function Home() {
             </code>
             .
           </li>
-          <li className="tracking-[-.01em]">Push code to see pipeline.</li>
+          <li className="tracking-[-.01em]">
+            Push code to see pipeline change or workflow on jenkins
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
